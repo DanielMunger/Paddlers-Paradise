56708598762c4484ecf0e9f23fdc5c054943cc5c
Daniel Munger <mungerda@gmail.com>
2017-03-08 14:11:13 -0800

    add google maps, marker and text box.

--- src/PaddlersParadise/wwwroot/js/GaugeDetails.js
+++ src/PaddlersParadise/wwwroot/js/GaugeDetails.js
@@ -2,8 +2,8 @@
     var url = window.location.pathname;
     var urlArray = url.split('/');
     var gaugeId = urlArray[3];
-    console.log(gaugeId)
-
+    
+    
     $.ajax({
         url: "/RiverRun/USGSCall/",
         type: 'POST',
@@ -17,6 +17,9 @@
             var state = site[1]
             var agency = result.value.timeSeries[0].sourceInfo.siteCode[0].agencyCode
             var network = result.value.timeSeries[0].sourceInfo.siteCode[0].network
+            var latitude = result.value.timeSeries[0].sourceInfo.geoLocation.geogLocation.latitude
+            var longitude = result.value.timeSeries[0].sourceInfo.geoLocation.geogLocation.longitude
+            initMap(latitude,longitude, gaugeName)
             if (unit == "deg C") {
                 unit = "Gage height, ft"
             }
@@ -34,3 +37,56 @@
         }
     });
 });
+
+var initMap = function (latitude, longitude, gaugeName) {
+    if (latitude != null && longitude != null)
+    {
+   
+    var center = { lat: latitude, lng: longitude };
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 13,
+        center: center,
+        mapTypeId: google.maps.MapTypeId.TERRAIN
+    });
+        google.maps.event.addListenerOnce(map, 'idle', function () {
+            google.maps.event.trigger(map, 'resize');
+           
+        });
+
+    var marker = new google.maps.Marker({
+        position: center,
+        map: map,
+        title: 'Marker Title'
+    });
+    var contentString = '<div id="content">' +
+            '<div id="siteNotice">' +
+            '</div>' +
+            '<h5 id="firstHeading" class="firstHeading">'+gaugeName+'</h5>' +
+            '<div id="bodyContent">' +
+            '<p></p>' +
+            '</div>' +
+            '</div>';
+    var infoWindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+    marker.addListener('click', function () {
+        infoWindow.open(map, marker);
+    });
+
+    //function addMarker(feature) {
+    //    var marker = new google.maps.Marker({
+    //        position: feature.position,
+    //        map: map
+    //    })
+    //}
+
+    //var features = [
+    //    {
+    //        position: new google.maps.LatLng(45, -122)
+    //    }
+    //]
+    //for (var i = 0, feature; feature = features[i]; i++) {
+    //    addMarker(feature);
+    //}
+    }
+}
