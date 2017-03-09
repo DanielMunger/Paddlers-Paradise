471d4b95e6f82dfd490f8d80baa7421cd9d654b1
Daniel Munger <mungerda@gmail.com>
2017-03-09 11:16:02 -0800

    add admin controller.

---
+++ src/PaddlersParadise/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PaddlersParadise.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Diagnostics;
+
+namespace PaddlersParadise.Controllers
+{
+    public class AdminController : Controller
+    {
+       
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
